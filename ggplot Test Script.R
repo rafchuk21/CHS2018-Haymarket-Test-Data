aa4c8d3d5fa557a75fbfd6682ba3faed5a84627c
Noah Gleason <noah.gleason1@gmail.com>
2018-03-08 15:17:14 -0500

    added a method to remove data based on columns, to be used for no show/dead matches

--- ggplot Test Script.R
+++ ggplot Test Script.R
@@ -7,10 +7,39 @@ if (!exists("originalEventDataSheet")) {
   originalEventDataSheet <- read_excel("~/R/CHS2018-Haymarket-Test-Data/Haymarket Week 1 Data.xlsx")
 }
 
+AND <- function(...) {
+  conditions = list(...)
+  ret <- TRUE
+  for (i in conditions) {
+    ret <- ret & i
+  }
+  return(ret)
+}
+
 cleanData <- function(dataSet = originalEventDataSheet, relevantData) {
-  ret = dataSet[,relevantData]
+  ret = dataSet[,unique(c(relevantData, excludeRow))]
   ret <- cleanNA(ret)
   colnames(ret) <- gsub(x = colnames(ret), pattern = " ", replacement = "")
+  return(ret)
+}
+
+#if a row has a "1" in any of its dataExclusionConditions columns, it is removed from the dataset
+excludeRows <- function(dataSet, dataExclusionConditions) {
+  ret = dataSet
+  i <- 1
+  while(i <= nrow(ret)) {
+    for (condition in dataExclusionConditions) {
+      print(i)
+      print(condition)
+      if (ret[i,condition] > 0) {
+        print("cond true")
+        ret <- ret[-1*i,]
+        i <- i-1
+        break
+      }
+    }
+    i <- i+1
+  }
   return(ret)
 }
 
